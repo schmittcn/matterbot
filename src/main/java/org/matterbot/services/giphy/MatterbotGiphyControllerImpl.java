4973fdfc86f8c3aa787cd00277bb75c5e6001084
Gino <luigi.von.alkier@icloud.com>
2021-10-29 19:13:38 +0200

    added giphy interface

--- src/main/java/org/matterbot/services/giphy/MatterbotGiphyControllerImpl.java
+++ src/main/java/org/matterbot/services/giphy/MatterbotGiphyControllerImpl.java
@@ -1,18 +1,19 @@
 package org.matterbot.services.giphy;
 
-import java.io.IOException;
-import java.util.List;
 import org.matterbot.mattermost.MattermostServiceImpl;
 import org.matterbot.services.URLQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MatterbotGiphyControllerImpl {
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class MatterbotGiphyControllerImpl implements MatterbotGiphyController {
     private final URLQueryService giphyService;
     private final MattermostServiceImpl mattermostServiceImpl;
 
@@ -28,9 +29,9 @@ public class MatterbotGiphyControllerImpl {
     public ResponseEntity<String> postGiphyTrendingMessage() throws IOException {
         var giphyUrl = giphyService.getUrl(URLQueryService.Strategy.TRENDING);
         GiphyMessageImpl giphyMessageImpl = GiphyMessageImpl.builder()
-                                                            .caption("trending")
-                                                            .giphyUrl(giphyUrl)
-                                                            .build();
+                .caption("trending")
+                .giphyUrl(giphyUrl)
+                .build();
         return mattermostServiceImpl.sendMessage(giphyMessageImpl);
     }
 
@@ -38,9 +39,9 @@ public class MatterbotGiphyControllerImpl {
     public ResponseEntity<String> postGiphyRandomMessage() throws IOException {
         var giphyUrl = giphyService.getUrl(URLQueryService.Strategy.RANDOM);
         return mattermostServiceImpl.sendMessage(GiphyMessageImpl.builder()
-                                                                 .caption("random")
-                                                                 .giphyUrl(giphyUrl)
-                                                                 .build());
+                .caption("random")
+                .giphyUrl(giphyUrl)
+                .build());
     }
 
     @GetMapping("/call/giphy/search")
@@ -55,9 +56,9 @@ public class MatterbotGiphyControllerImpl {
             giphyUrl = giphyService.getUrl(URLQueryService.Strategy.SEARCH, query);
         }
         return mattermostServiceImpl.sendMessage(GiphyMessageImpl.builder()
-                                                                 .caption(query)
-                                                                 .giphyUrl(giphyUrl)
-                                                                 .build());
+                .caption(query)
+                .giphyUrl(giphyUrl)
+                .build());
     }
 
     @GetMapping(value = "/call/giphy/search2", produces = "application/json")
