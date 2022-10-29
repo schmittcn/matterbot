bcd83e5be25d9190f61764a7c61e7d37de70991a
Gino <luigi.von.alkier@icloud.com>
2022-10-29 18:56:55 +0200

    mattermost cleanup
    removed unused dto fields in mattermost package

--- src/main/java/org/matterbot/mattermost/types/Attachments.java
+++ src/main/java/org/matterbot/mattermost/types/Attachments.java
@@ -1,16 +1,15 @@
 package org.matterbot.mattermost.types;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Attachments {
     private String title;
     private String title_link;
-
-    private String pretext;
     private String text;
-
     private String image_url;
 }
--- src/main/java/org/matterbot/mattermost/types/MattermostAttachmentMessage.java
+++ src/main/java/org/matterbot/mattermost/types/MattermostAttachmentMessage.java
@@ -1,13 +1,15 @@
 package org.matterbot.mattermost.types;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MattermostAttachmentMessage {
-    private String channel_id;
     private String username;
     private String icon_url;
     private String message;
